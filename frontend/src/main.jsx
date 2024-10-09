15f6c6af8302ae145a6723f17291b770ffe946a6
nicolas2289h <nicolas2289h@gmail.com>
2024-10-08 21:24:41 -0300

    feat: add new library PrimeReact for styled components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import { PrimeReactProvider } from 'primereact/api';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <PrimeReactProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </PrimeReactProvider>
 )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/primereact/**/*.{js,ts,jsx,tsx}",  // Incluye las clases de PrimeReact
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'soft': '0 0 15px rgba(0, 0, 0, 0.2)',
+      }
+    },
   },
   plugins: [],
-}
+};
