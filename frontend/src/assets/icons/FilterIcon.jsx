847ea22f10ad4ceac66b55a7f85ca83e925a226e
nicolas2289h <nicolas2289h@gmail.com>
2024-10-08 21:28:58 -0300

    feat: add new icons and football team logos for League component

---
+++ frontend/src/assets/icons/FilterIcon.jsx
@@ -0,0 +1,11 @@
+const FilterIcon = () => {
+    return (
+        <svg width="24" height="18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M6.26489 1.66309V16.5851" stroke="#8E2BFF" strokeWidth="1.57534" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M11.1064 11.4419L6.26489 16.5851L1.42334 11.4419" stroke="#8E2BFF" strokeWidth="1.57534" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M17.6074 16.585V1.66297" stroke="#8E2BFF" strokeWidth="1.57534" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M22.449 6.80615L17.6074 1.66298L12.7659 6.80615" stroke="#8E2BFF" strokeWidth="1.57534" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+export default FilterIcon
