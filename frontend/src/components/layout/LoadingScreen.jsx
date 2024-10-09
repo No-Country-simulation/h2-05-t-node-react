f67742c5beb8d72a0d31d6cf7fc56cb737aef1f0
nicolas2289h <nicolas2289h@gmail.com>
2024-10-08 21:25:15 -0300

    refactor: add new progress bar

--- frontend/src/components/layout/LoadingScreen.jsx
+++ frontend/src/components/layout/LoadingScreen.jsx
@@ -1,8 +1,11 @@
+import 'primereact/resources/themes/nano/theme.css'
+import { ProgressBar } from "primereact/progressbar"
+
 const LoadingScreen = () => {
     return (
         <main className='h-screen w-full flex flex-col bg-gradient-to-r from-[#317EF4] to-[#8E2BFF] justify-center items-center'>
             <h1 className="text-[44.5px] font-black text-white">WAKI</h1>
-            <div className="w-[134px] h-[5px] absolute bottom-3 bg-white rounded-lg"></div>
+            <ProgressBar className="h-[5px] w-[134px] absolute bottom-3 rounded-lg" mode="indeterminate"></ProgressBar>
         </main>
     )
 }
