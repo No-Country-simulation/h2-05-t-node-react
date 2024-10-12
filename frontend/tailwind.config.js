4dbd6ce1dad47bf2e99a9fe44a397f658af77d03
nicolas2289h <nicolas2289h@gmail.com>
2024-10-12 14:27:02 -0300

    styles: add poppins font and update tailwind.config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,9 +7,25 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'gradiente': 'linear-gradient(to right, #317EF4, #8E2BFF)',
+      },
       boxShadow: {
         'soft': '0 0 15px rgba(0, 0, 0, 0.2)',
         'soft-md': '0 1px 1px rgba(0,0,0,0.1)'
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      fontSize: {
+        'regular': '12px',
+        'title': '22px'
+      },
+      colors: {
+        'purple': '#8E2BF',
+        'blue': '#317EF4',
+        'primary': '#C5C5C5',
+        'secondary': '#555555'
       }
     },
   },
