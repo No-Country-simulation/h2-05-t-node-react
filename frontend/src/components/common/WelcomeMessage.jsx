6d05f1a3936d68b72609d8b6c5b6836540a29a0a
nicolas2289h <nicolas2289h@gmail.com>
2024-10-12 14:38:08 -0300

    styles: apply new styles and add new arrow icon

--- frontend/src/components/common/WelcomeMessage.jsx
+++ frontend/src/components/common/WelcomeMessage.jsx
@@ -1,7 +1,7 @@
 const WelcomeMessage = ({ title, message }) => {
     return (
         <section className='mt-10 w-[80%] mx-auto'>
-            <h1 className="text-2xl font-semibold text-blue-500">{title}</h1>
+            <h1 className="text-2xl font-semibold text-blue">{title}</h1>
             <p className="mt-1 text-sm">{message}</p>
         </section>
     )
--- frontend/src/components/layout/LoadingScreen.jsx
+++ frontend/src/components/layout/LoadingScreen.jsx
@@ -3,7 +3,7 @@ import { ProgressBar } from "primereact/progressbar"
 
 const LoadingScreen = () => {
     return (
-        <main className='h-screen w-full flex flex-col bg-gradient-to-r from-[#317EF4] to-[#8E2BFF] justify-center items-center'>
+        <main className='h-screen w-full flex flex-col bg-gradiente justify-center items-center'>
             <h1 className="text-[44.5px] font-black text-white">WAKI</h1>
             <ProgressBar className="h-[5px] w-[134px] absolute bottom-3 rounded-lg" mode="indeterminate"></ProgressBar>
         </main>
