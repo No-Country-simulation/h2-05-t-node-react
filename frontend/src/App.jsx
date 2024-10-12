cc9b2588cf2cfcfdd84201955f12ab2eb7f44473
nicolas2289h <nicolas2289h@gmail.com>
2024-10-12 14:43:42 -0300

    feat: add NotFoundPage and new routes to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,10 @@ import './index.css'
 import LoadingScreen from './components/layout/LoadingScreen'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from "./pages/LoginPage"
-import PredictionsPage from './pages/PredictionsPage';
 import RegisterPage from "./pages/RegisterPage"
+import MatchesPage from './pages/MatchesPage';
+import NotFoundPage from './pages/NotFoundPage';
+import PredictionsPage from './pages/PredictionsPage';
 
 function App() {
 
@@ -15,7 +17,9 @@ function App() {
           <Route path="/" element={<LoadingScreen />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/matches" element={<MatchesPage />} />
           <Route path="/predictions" element={<PredictionsPage />} />
+          <Route path="*" element={<NotFoundPage />} />
      
         </Routes>
       </div>
