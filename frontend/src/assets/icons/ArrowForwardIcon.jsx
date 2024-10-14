cbc18114293886efad1ed463de767b004b644fe5
nicolas2289h <nicolas2289h@gmail.com>
2024-10-14 16:49:11 -0300

    feat: add new icons

---
+++ frontend/src/assets/icons/ArrowForwardIcon.jsx
@@ -0,0 +1,10 @@
+const ArrowForwardIcon = () => {
+    return (
+        <svg width="17" height="12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1.09375 6.28308L16.0157 6.28308" stroke="#8E2BFF" strokeWidth="1.57534" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M10.8726 10.8981L16.0157 6.2832L10.8726 1.66833" stroke="#8E2BFF" strokeWidth="1.57534" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+
+    )
+}
+export default ArrowForwardIcon
