544ea1b29c01fd3d856c6dcb2a25ea89bae29426
nicolas2289h <nicolas2289h@gmail.com>
2024-10-16 22:55:26 -0300

    feat: new page for completed matches

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,27 +7,37 @@ import MatchesPage from './pages/MatchesPage';
 import NotFoundPage from './pages/NotFoundPage';
 import PredictionsPage from './pages/PredictionsPage';
 import MatchesCompletedPage from './pages/MatchesCompletedPage';
+import DetailsPage from './pages/DetailsPage';
+import CompletedPredictionsPage from './pages/CompletedPredictionsPage';
+import ClassificationPage from './pages/ClassificationPage';
 
 function App() {
 
   return (
     <Router>
-    <div className="App">
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<LoadingScreen />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/matches" element={<MatchesPage />} />
-          <Route path="/matches-completed" element={<MatchesCompletedPage />} />
-          <Route path="/predictions" element={<PredictionsPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-     
-        </Routes>
+      <div className="App">
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<LoadingScreen />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/matches" element={<MatchesPage />} />
+
+            <Route path="/matches-completed" element={<MatchesCompletedPage />}>
+              <Route index element={<CompletedPredictionsPage />} />
+              <Route path='predictions' element={<CompletedPredictionsPage />} />
+              <Route path='details' element={<DetailsPage />} />
+              <Route path='classification' element={<ClassificationPage />} />
+            </Route>
+
+            <Route path="/predictions" element={<PredictionsPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+
+          </Routes>
+        </div>
+
       </div>
-    
-    </div>
-  </Router>
+    </Router>
 
   )
 }
--- frontend/src/pages/MatchesCompletedPage.jsx
+++ frontend/src/pages/MatchesCompletedPage.jsx
@@ -1,12 +1,11 @@
-import { Link, NavLink } from "react-router-dom"
+import { Link, NavLink, Outlet } from "react-router-dom"
 import ArrowBackBlueIcon from '../assets/icons/ArrowBackBlueIcon'
-import ArrowForwardIcon from '../assets/icons/ArrowForwardIcon'
 import BarcelonaImg from '../assets/img/barcelona.png'
 import RealMadridImg from '../assets/img/realMadrid.png'
 
 const MatchesCompletedPage = () => {
     return (
-        <main className="h-[286px]" >
+        <main>
             <section className="w-[90%] mx-auto flex flex-col">
                 <Link to='/matches' className="w-[90px] flex items-center gap-2 mt-7 text-blue text-sm p-2 ps-0">
                     <ArrowBackBlueIcon />
@@ -33,67 +32,23 @@ const MatchesCompletedPage = () => {
 
             <nav className='flex h-[53px] px-5 shadow-md'>
                 <NavLink
-                    to='/matches-completed'
-                    // className={({ isActive }) => `w-[50%] flex justify-center items-center font-medium cursor-pointer border-b-[3.5px] ${isActive ? 'text-blue border-b-blue' : 'border-b-transparent'}`}>
-                    className='w-[50%] flex justify-center items-center text-secondary font-medium cursor-pointer'>
+                    to='predictions'
+                    className={({ isActive }) => `w-[50%] flex justify-center items-center font-medium cursor-pointer border-b-[3.5px] ${isActive || window.location.pathname === '/matches-completed' ? 'text-blue border-b-blue' : 'border-b-transparent'}`}>
                     Predicciones
                 </NavLink>
                 <NavLink
-                    to='/matches-completed'
-                    // className={({ isActive }) => `w-[50%] flex justify-center items-center font-medium cursor-pointer border-b-[3.5px] ${isActive ? 'text-blue border-b-blue' : 'border-b-transparent'}`}>
-                    className='w-[50%] flex justify-center items-center text-secondary font-medium cursor-pointer'>
+                    to='details'
+                    className={({ isActive }) => `w-[50%] flex justify-center items-center font-medium cursor-pointer border-b-[3.5px] ${isActive ? 'text-blue border-b-blue' : 'border-b-transparent'}`}>
                     Detalles
                 </NavLink>
                 <NavLink
-                    to='/matches-completed'
-                    // className={({ isActive }) => `w-[50%] flex justify-center items-center font-medium cursor-pointer border-b-[3.5px] ${isActive ? 'text-blue border-b-blue' : 'border-b-transparent'}`}>
-                    className='w-[50%] flex justify-center items-center text-secondary font-medium cursor-pointer'>
+                    to='classification'
+                    className={({ isActive }) => `w-[50%] flex justify-center items-center font-medium cursor-pointer border-b-[3.5px] ${isActive ? 'text-blue border-b-blue' : 'border-b-transparent'}`}>
                     Clasificación
                 </NavLink>
             </nav>
 
-            <section className="w-[90%] mx-auto flex flex-col gap-3 mt-7">
-                <div className="flex justify-between py-2">
-                    <span className="font-medium">Tus predicciones</span>
-                    <div className="flex gap-2 items-center">
-                        <span className="text-purple">Ver todas</span>
-                        <ArrowForwardIcon />
-                    </div>
-                </div>
-
-                <div className="flex justify-between items-center shadow-soft rounded-xl py-2 px-5">
-                    <div className="flex flex-col text-regular-14">
-                        <span>No hiciste predicciones</span>
-                        <span className="text-tertiary">Suerte la próxima</span>
-                    </div>
-                    <span className="text-blue">0 puntos</span>
-                </div>
-
-
-
-                <div className="py-2 mt-1">
-                    <span className="font-medium">Pronóstico general</span>
-                </div>
-
-                <div className="flex flex-col justify-center shadow-soft rounded-xl">
-                    <span className="py-2 px-5 text-secondary text-sm">Resultado final</span>
-
-                    <div className="w-full bg-soft-gray flex justify-center gap-10 py-5">
-                        <div className="flex flex-col items-center">
-                            <span className="text-regular text-secondary">Osasuna</span>
-                            <span className="text-title font-semibold text-blue mt-1.5">48%</span>
-                        </div>
-                        <div className="flex flex-col items-center">
-                            <span className="text-regular text-secondary">Empate</span>
-                            <span className="text-title font-semibold text-blue mt-1.5">12%</span>
-                        </div>
-                        <div className="flex flex-col items-center">
-                            <span className="text-regular text-secondary">Barcelona</span>
-                            <span className="text-title font-semibold text-blue mt-1.5">40%</span>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <Outlet />
         </main >
     )
 }
