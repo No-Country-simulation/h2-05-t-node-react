9713c5df394172c792164400b656afbf03fd5515
nicolas2289h <nicolas2289h@gmail.com>
2024-10-28 13:31:41 -0300

    refactor: update football match components

--- frontend/src/pages/ClassificationPage.jsx
+++ frontend/src/pages/ClassificationPage.jsx
@@ -37,7 +37,138 @@ const ClassificationPage = () => {
           </div>
 
           <div>
-            <span className='text-regular'>Dom 28-10</span>
+            {/* COMPLETAR CONTENIDO */}
+            <span className='text-regular'></span> 
+          </div>
+        </div>
+
+
+
+
+
+
+
+
+
+        {/* BORRAR DUPLICADOS */}
+
+        <div className='w-full bg-soft-gray py-3 flex justify-around gap-10'>
+          <div className="flex flex-col items-start gap-1">
+            <div className='flex gap-2'>
+              <span className="text-regular text-secondary capitalize">Barcelona</span>
+            </div>
+            <div className='flex gap-2'>
+              <span className="text-regular text-secondary capitalize">Real Madrid</span>
+            </div>
+          </div>
+
+          <div className='flex items-center gap-3'>
+            <div className="flex flex-col items-center">
+              <span className="text-regular">3</span>
+              <span className="text-regular">3</span>
+            </div>
+            <FlagIcon />
+          </div>
+
+          <div>
+            {/* COMPLETAR CONTENIDO */}
+            <span className='text-regular'></span> 
+          </div>
+        </div>
+
+        <div className='w-full bg-soft-gray py-3 flex justify-around gap-10'>
+          <div className="flex flex-col items-start gap-1">
+            <div className='flex gap-2'>
+              <span className="text-regular text-secondary capitalize">Barcelona</span>
+            </div>
+            <div className='flex gap-2'>
+              <span className="text-regular text-secondary capitalize">Real Madrid</span>
+            </div>
+          </div>
+
+          <div className='flex items-center gap-3'>
+            <div className="flex flex-col items-center">
+              <span className="text-regular">3</span>
+              <span className="text-regular">3</span>
+            </div>
+            <FlagIcon />
+          </div>
+
+          <div>
+            {/* COMPLETAR CONTENIDO */}
+            <span className='text-regular'></span> 
+          </div>
+        </div>
+
+        <div className='w-full bg-soft-gray py-3 flex justify-around gap-10'>
+          <div className="flex flex-col items-start gap-1">
+            <div className='flex gap-2'>
+              <span className="text-regular text-secondary capitalize">Barcelona</span>
+            </div>
+            <div className='flex gap-2'>
+              <span className="text-regular text-secondary capitalize">Real Madrid</span>
+            </div>
+          </div>
+
+          <div className='flex items-center gap-3'>
+            <div className="flex flex-col items-center">
+              <span className="text-regular">3</span>
+              <span className="text-regular">3</span>
+            </div>
+            <FlagIcon />
+          </div>
+
+          <div>
+            {/* COMPLETAR CONTENIDO */}
+            <span className='text-regular'></span> 
+          </div>
+        </div>
+
+        <div className='w-full bg-soft-gray py-3 flex justify-around gap-10'>
+          <div className="flex flex-col items-start gap-1">
+            <div className='flex gap-2'>
+              <span className="text-regular text-secondary capitalize">Barcelona</span>
+            </div>
+            <div className='flex gap-2'>
+              <span className="text-regular text-secondary capitalize">Real Madrid</span>
+            </div>
+          </div>
+
+          <div className='flex items-center gap-3'>
+            <div className="flex flex-col items-center">
+              <span className="text-regular">3</span>
+              <span className="text-regular">3</span>
+            </div>
+            <FlagIcon />
+          </div>
+
+          <div>
+            {/* COMPLETAR CONTENIDO */}
+            <span className='text-regular'></span> 
+          </div>
+        </div>
+
+        <div className='w-full bg-soft-gray py-3 flex justify-around gap-10'>
+          <div className="flex flex-col items-start gap-1">
+            <div className='flex gap-2'>
+              <span className="text-regular text-secondary capitalize">Barcelona</span>
+            </div>
+            <div className='flex gap-2'>
+              <span className="text-regular text-secondary capitalize">Real Madrid</span>
+            </div>
+          </div>
+
+          <div className='flex items-center gap-3'>
+            <div className="flex flex-col items-center">
+              <span className="text-regular">3</span>
+              <span className="text-regular">3</span>
+            </div>
+            <FlagIcon />
+          </div>
+
+          <div>
+            {/* COMPLETAR CONTENIDO */}
+            <span className='text-regular'></span> 
           </div>
         </div>
       </div>
--- frontend/src/pages/MatchesCompletedPage.jsx
+++ frontend/src/pages/MatchesCompletedPage.jsx
@@ -1,19 +1,23 @@
-import { Link, NavLink, Outlet } from "react-router-dom"
-import ArrowBackBlueIcon from '../assets/icons/ArrowBackBlueIcon'
 import { useEffect, useState } from "react"
+import { Link, NavLink, Outlet } from "react-router-dom"
+import { formatDateToMD } from '../utils/formatDateToMD'
+import DefaultTeam from '../assets/img/defaultTeam.png'
+import ArrowBackBlueIcon from '../assets/icons/ArrowBackBlueIcon'
+import Footer from "../components/layout/Footer"
 
 const MatchesCompletedPage = () => {
     const [completedMatch, setCompletedMatch] = useState(null)
+    let date = formatDateToMD(completedMatch?.match_date)
 
     useEffect(() => {
-        const storedMatch = localStorage.getItem('completedMatch');
+        const storedMatch = localStorage.getItem('completedMatch')
         if (storedMatch) {
-            setCompletedMatch(JSON.parse(storedMatch));
+            setCompletedMatch(JSON.parse(storedMatch))
         }
-    }, []);
+    }, [])
 
     return (
-        <main>
+        <main className="flex flex-col min-h-screen">
             <section className="w-[90%] mx-auto flex flex-col">
                 <Link onClick={() => localStorage.clear()} to='/matches' className="w-[90px] flex items-center gap-2 mt-7 text-blue text-sm p-2 ps-0">
                     <ArrowBackBlueIcon />
@@ -22,18 +26,33 @@ const MatchesCompletedPage = () => {
 
                 <div className="flex items-center py-[18px] justify-around gap-8 text-center">
                     <div className="w-[83px]">
-                        <img className="mx-auto w-[88px] h-[88px] object-contain" src={completedMatch?.team_home_badge} alt={`Img ${completedMatch?.awayTeam}`} />
-                        <p className="capitalize text-secondary text-xs mt-[2px]">{completedMatch?.homeTeam}</p>
+                        <img className="mx-auto w-[88px] h-[88px] object-contain" src={completedMatch?.team_home_badge} alt={`Img ${completedMatch?.awayTeam}`} onError={(e) => { e.target.src = DefaultTeam }} />
+                        <p className="capitalize text-secondary text-xs mt-[2px] max-w-[80px] truncate overflow-hidden whitespace-nowrap">{completedMatch?.homeTeam}</p>
                     </div>
 
                     <div className="flex flex-col mb-3 justify-top">
-                        <span className="text-base text-secondary font-semibold capitalize">finalizado</span>
-                        <span className="text-medium ">{`${completedMatch?.hometeam_score} - ${completedMatch?.awayteam_score}`}</span>
+                        <span className="text-base text-secondary font-semibold capitalize">
+                            {/* ESTADO DEL PARTIDO */}
+                            {
+                                completedMatch?.match_status == 'Finished' ? 'Finalizado'
+                                    : completedMatch?.match_status == '' ? date
+                                        : completedMatch?.match_status == 'Half Time' ? 'Medio tiempo'
+                                            : `🔴 ` + completedMatch?.match_status + `'`
+                            }
+                        </span>
+                        <span className="text-medium ">
+                            {
+                                completedMatch?.match_status == '' ?
+                                    `10:30` //hh:mm
+                                    :
+                                    `${completedMatch?.hometeam_score} - ${completedMatch?.awayteam_score}`
+                            }
+                        </span>
                     </div>
 
                     <div className="w-[83px]">
-                        <img className="mx-auto w-[88px] h-[88px] object-contain" src={completedMatch?.team_away_badge} alt={`Img ${completedMatch?.awayTeam}`} />
-                        <p className="capitalize text-secondary text-xs mt-[2px]">{completedMatch?.awayTeam}</p>
+                        <img className="mx-auto w-[88px] h-[88px] object-contain" src={completedMatch?.team_away_badge} alt={`Img ${completedMatch?.awayTeam}`} onError={(e) => { e.target.src = DefaultTeam }} />
+                        <p className="capitalize text-secondary text-xs mt-[2px] max-w-[80px] truncate overflow-hidden whitespace-nowrap">{completedMatch?.awayTeam}</p>
                     </div>
                 </div>
             </section>
@@ -56,7 +75,11 @@ const MatchesCompletedPage = () => {
                 </NavLink>
             </nav>
 
-            <Outlet />
+            <div className="flex-grow">
+                <Outlet />
+            </div>
+
+            <Footer />
         </main >
     )
 }
