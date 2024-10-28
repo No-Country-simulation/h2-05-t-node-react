bead1d326bbaf9f6f48d7b0b937eb93d462e0b3b
nicolas2289h <nicolas2289h@gmail.com>
2024-10-28 13:33:15 -0300

    feat: add new icon and route to footer

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -2,29 +2,36 @@ import { NavLink } from 'react-router-dom';
 import ScoutIcon from '../../assets/icons/ScoutIcon';
 import CupIcon from '../../assets/icons/CupIcon';
 import BallIcon from '../../assets/icons/BallIcon';
+import HomeIcon from '../../assets/icons/HomeIcon';
 
 const Footer = () => {
     return (
-        <footer className="flex justify-center gap-11 px-10 pt-4 pb-5 w-full bg-blue rounded-t-lg mt-5 text-xs font-medium tracking-tight text-center">
+        <footer className="flex justify-center gap-9 pt-4 pb-5 w-full bg-blue rounded-t-lg mt-5 text-xs font-medium tracking-tight text-center">
+            <NavLink
+                to='/'
+                className={({ isActive }) => `flex flex-col items-center ${isActive ? 'text-white' : 'text-opacity-30 text-white'}`}>
+                <HomeIcon className={({ isActive }) => isActive ? 'text-white' : 'text-opacity-30 text-white'} />
+                <p className='mt-[11px] text-regular'>Home</p>
+            </NavLink>
             <NavLink
                 to='/scout-players'
-                className={({ isActive }) => `flex flex-col items-center transition duration-500 ${isActive ? 'text-white' : 'text-opacity-30 text-white'}`}>
+                className={({ isActive }) => `flex flex-col items-center ${isActive ? 'text-white' : 'text-opacity-30 text-white'}`}>
                 <ScoutIcon className={({ isActive }) => isActive ? 'text-white' : 'text-opacity-30 text-white'} />
-                <p className='mt-2.5'>Scout players</p>
+                <p className='mt-[11px] text-regular'>Scout players</p>
             </NavLink>
 
             <NavLink
                 to='/matches'
-                className={({ isActive }) => `flex flex-col items-center transition duration-500 ${isActive ? 'text-white' : 'text-opacity-30 text-white'}`}>
+                className={({ isActive }) => `flex flex-col items-center ${isActive ? 'text-white' : 'text-opacity-30 text-white'}`}>
                 <BallIcon className={({ isActive }) => isActive ? 'text-white' : 'text-opacity-30 text-white'} />
-                <p className='mt-2.5'>Partidos</p>
+                <p className='mt-[11px] text-regular'>Partidos</p>
             </NavLink>
 
             <NavLink
                 to='/divisions'
-                className={({ isActive }) => `flex flex-col items-center transition duration-500 ${isActive ? 'text-white' : 'text-opacity-30 text-white'}`}>
+                className={({ isActive }) => `flex flex-col items-center ${isActive ? 'text-white' : 'text-opacity-30 text-white'}`}>
                 <CupIcon className={({ isActive }) => isActive ? 'text-white' : 'text-opacity-30 text-white'} />
-                <p className='mt-2.5'>Divisiones</p>
+                <p className='mt-[11px] text-regular'>Divisiones</p>
             </NavLink>
         </footer>
     );
